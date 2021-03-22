0400efe0af28c4ac94c56073759ada55ab907585
GutsAndBlood <ajgo_24@hotmail.com>
2021-03-21 21:05:15 -0600

    fixing header links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import Logo from '../../Images/logo.png'
 import './Header.css'
 
 
 const Header = (props) => { 
 
-
     return (
         <header className="mainDivHeader">
            <a href='/react-portfolio/'> <img src={Logo} className='mainLogo' alt='logo'/></a> 
 
-           <a href={props.AboutMe} className='aboutMeHeader'>About me</a> 
-           <a href="/react-portfolio/contact" className='contact'>Contact</a> 
+           <Link to={props.AboutMe} className='aboutMeHeader'>About me</Link> 
+           <Link to="/react-portfolio/contact" className='contact'>Contact</Link> 
         </header>
     )
 
