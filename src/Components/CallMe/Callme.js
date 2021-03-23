34f1dacbf313d885893c3b3bbc78d3d1c476086c
GutsAndBlood <GutsAndBlood@github.com>
2021-03-22 23:04:32 -0600

    done with callme seccion and contact v1.0

--- src/Components/CallMe/Callme.js
+++ src/Components/CallMe/Callme.js
@@ -8,13 +8,11 @@ const Callme = () => {
 
     return(
         <section className='callMeBox'>
-            <div>
                 <div className="callMeFlex">
                     <h3>Start a Project</h3>
                     <p>Interested in working together? Let&apos;s schedule a call</p>
                     <button onClick={() => history.push('/react-portfolio/contact')} className="buttonTalk">Let&apos;s Talk</button>
                 </div>
-            </div>
         </section>
     )
 
